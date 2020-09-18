7093037195f752315453769d4e9978bdbe054d03
arthurramires <arthurramires1802@outlook.com>
2020-09-18 12:34:02 -0400

    Configurando stress do server para teste

--- src/server.js
+++ src/server.js
@@ -1,13 +1,3 @@
 import app from './app';
-var stress = require('stress-node');
-stress({
-	url: 'http://google.com',
-	method: 'GET',
-	amount: 6000,
-	timeout: 600000, 
-	concurrent: 30,
-	data: null,
-}, function(report) {
-});
 
 app.listen(3333);
